4f735fc40715e43d2e6d87f33aaab31d7c2b63e9
Sergey Yaskov <yaskovdev@gmail.com>
2017-11-20 12:45:49 +0200

    Improve the code.

--- mushrooms-knn/src/main/scala/ManualEvaluation.scala
+++ mushrooms-knn/src/main/scala/ManualEvaluation.scala
@@ -1,33 +1,25 @@
 import scala.io.{Source, StdIn}
 
-object BatchEvaluation {
+object ManualEvaluation {
 
   def main(args: Array[String]): Unit = {
     println("Enter a mushroom description:")
     val string = StdIn.readLine()
     val featureVector: Seq[String] = string.split(",").toSeq
     val data = read("training.data")
-    println(closest(featureVector.tail, data).head)
+    println(nearest(featureVector.tail, data).head)
   }
 
   private def read(resource: String): Seq[Seq[String]] = {
     Source.fromResource(resource).getLines().map(_.split(",").toSeq).toSeq
   }
 
-  private def closest(y: Seq[String], xs: Seq[Seq[String]]): Seq[String] = {
-    var candidate: Seq[String] = Seq()
-    var currentDistance: Double = Double.MaxValue
-    for (x <- xs) {
-      val newDistance = distance(x.tail, y)
-      if (newDistance < currentDistance) {
-        currentDistance = newDistance
-        candidate = x
-      }
-    }
-    candidate
+  private def nearest(y: Seq[String], xs: Seq[Seq[String]]): Seq[String] = {
+    xs.sortWith((a, b) => distance(a.tail, y) < distance(b.tail, y)).head
   }
 
   private def distance(x: Seq[String], y: Seq[String]): Double = {
+    require(x.size == y.size, "mismatch between vector sizes")
     x.zip(y).foldLeft(0.0)((sum, pair) => if (pair._1 == pair._2) sum else sum + 1.0)
   }
 }
